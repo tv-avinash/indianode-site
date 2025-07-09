4a2e0130c3bf975f0332c4aedc79d3ca98a2174f
tv-avinash <tvavinash@gmail.com>
2025-07-09 23:56:13 +0530

    Update and rename main.jsx to src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./Home";
-import "./style.css";
+import Home from "./Home"; // Assuming Home.jsx is in the same directory
+import "./style.css"; // Optional if you're using global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
