428702cbd2f1e1722fd725151d78bd80033c4a35
tv-avinash <tvavinash@gmail.com>
2025-07-09 23:58:51 +0530

    Update RazorpayButton.jsx

--- src/components/RazorpayButton.jsx
+++ src/components/RazorpayButton.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
 const RazorpayButton = () => {
-  return (
-    <form>
-      <script
-        src="https://checkout.razorpay.com/v1/payment-button.js"
-        data-payment_button_id="pl_Qr3mbIECabKEfb"
-        async>
-      </script>
-    </form>
-  );
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://checkout.razorpay.com/v1/payment-button.js";
+    script.setAttribute("data-payment_button_id", "pl_Qr3mbIECabKEfb");
+    script.async = true;
+
+    const form = document.getElementById("razorpay-form");
+    form.appendChild(script);
+
+    return () => {
+      form.innerHTML = ""; // Clean up when component unmounts
+    };
+  }, []);
+
+  return <form id="razorpay-form"></form>;
 };
 
 export default RazorpayButton;
